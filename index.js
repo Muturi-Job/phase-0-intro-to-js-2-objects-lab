2000db84c76cb46e1d61265c38b81342674ecf2b
Job Muturi <muirimuturi17@gmail.com>
2023-03-10 12:55:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Person1",
+    streetAddress: "Street1"
+};
+function updateEmployeeWithKeyAndValue(obj, key, value ){
+    const newObj = {...obj}
+    newObj[key] = value;
+    return newObj
+};
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name", 
+    "Person2"
+
+);
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    employee["name"] = "Sam";
+    employee["streetAddress"] = "12 Broadway";
+    return employee;
+    
+}
+function deleteFromEmployeeByKey(){
+    const newObj = {...employee};
+    delete newObj.name;
+    return newObj
+
+}
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    return employee;
+}
+
